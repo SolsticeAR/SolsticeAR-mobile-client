4a8a2fd4550f8bb799c79f2386587da8c92c3650
Jill <amnesiacsiblings@gmail.com>
2019-09-17 11:23:33 +0900

    commiting these changes before bundling the app again in case something goes wonky. I mostly just added styling to the splash page.

--- Splash.js
+++ Splash.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet, Modal, Image } from 'react-native';
 
 export default class Splash extends Component {
   
@@ -14,16 +14,33 @@ export default class Splash extends Component {
   render() {
     return (
       <View>
-        <View style={{height: 50}}>
-          <Text>Oi Oi this is a splash page, test me</Text>
-        </View>
-        <View>
-          <TouchableHighlight onPress={() => { this.onCampaignPress() }} style={localStyles.buttons} underlayColor={'#68a0ff'}>
-              <View>
-                <Text style={localStyles.buttonText}>TEST ME</Text>
-              </View>
-            </TouchableHighlight>
-        </View>
+        <Modal
+          animationType={"slide"}
+          transparent={true}
+          style={localStyles.splashContainer}
+        >
+          <View style={localStyles.splashContainer}>
+            <View style={localStyles.splashTitleContainer}>
+              <Text style={localStyles.splashTitle}>Solstice AR</Text>
+            </View>
+            <View style={localStyles.splashDescriptionContainer}>
+              <Text style={localStyles.splashTitle } allowFontScaling={true}>
+                Welcome to Solstice AR. A platform that allows users to both create and enjoy unique augmented reality experiences.
+              </Text>
+            </View>
+            <View>
+              <Image source={require('./icons/splashpage-icon.gif')} style={localStyles.logo}>
+              </Image>
+            </View>
+            <View style={localStyles.splashExitContainer}>
+              <TouchableHighlight onPress={() => { this.onCampaignPress() }} style={localStyles.buttons} underlayColor={'#68a0ff'}>
+                <View>
+                  <Text style={localStyles.buttonText}>TEST ME</Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -46,5 +63,53 @@ const localStyles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
-  }
+  },
+  logo:{
+    width: 100,
+    height: 100,
+		alignItems:'center',
+  },
+  splashContainer:{
+		backgroundColor:'white',
+		flex:1,
+		marginTop:70,
+		marginBottom:40,
+		marginLeft:20,
+		marginRight:20,
+		borderRadius:20,
+		borderWidth:4,
+		borderColor:'#68a0cf'
+	},
+	splashTitle:{
+		color:'#68a0cf',
+        fontWeight:'bold',
+		fontSize:20,
+		textAlign:'center',
+		margin:10,	
+	},
+	splashDescription:{
+		color:'#68a0cf',
+        fontSize:15,
+		marginRight:20,
+		marginLeft:20
+	},
+	splashCloseIcon:{
+		alignSelf:'flex-end',
+		flex:0.5,
+		marginRight:10
+	},
+	splashTitleContainer:{
+		flex:1,
+		flexDirection:'row',
+		justifyContent:'center',
+		alignItems:'center'
+	},
+	splashDescriptionContainer:{
+		flex:6.5
+	},
+	splashExitContainer:{
+		flex:2,
+		justifyContent:'flex-start',
+		alignItems:'center',
+	}
 });
